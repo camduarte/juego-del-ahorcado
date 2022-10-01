dbeb4cda26c3311022684e0fae2e5a49db597d5c
cduarte <duarte.camd@gmail.com>
2022-10-01 20:35:01 -0300

    Agrego funcionalidad al boton Iniciar juego. Agrego funcionalidad de escoger palabra secreta.

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -2,8 +2,13 @@
     let palabrasSecretas = ["HTML","CSS","JAVASCRIPT","PYTHON","CPLUSCPLUS","JAVA","COBOL","ASSEMBLY","SQL","PHP"];
     let palabraSecreta = "";
 
+    /**
+     * Escoge la palabra secreta.
+     */
     function escogerPalabraSecreta() {
         indiceAleatorio = Math.floor(Math.random() * palabrasSecretas.length);
         palabraSecreta = palabrasSecretas[indiceAleatorio];
     }
+
+    escogerPalabraSecreta();
 })()
